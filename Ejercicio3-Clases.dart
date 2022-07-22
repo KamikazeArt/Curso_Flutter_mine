4872a23f84476cb7cc89de7f460f85a76619054d
KamikazeArt <yail_valdespino@hotmail.com>
2022-07-21 21:05:29 -0500

    Create Ejercicio3-Clases.dart

---
+++ Ejercicio3-Clases.dart
@@ -0,0 +1,43 @@
+/* EJERCICIO 
+Crea una clase llamada Cuenta que tendrá los siguientes atributos:
+titular y cantidad (puede tener decimales).
+ - El titular será obligatorio y la cantidad es opcional. *
+ - Crea dos constructores(diferentes) que cumpla lo anterior.
+ - Crea sus métodos get, set y toString.
+Tendrá dos métodos especiales:
+1) ingresar(double cantidad): 
+se ingresa una cantidad a la cuenta, si la cantidad introducida es negativa, no se hará nada.
+2) retirar(double cantidad): 
+se retira una cantidad a la cuenta, si restando la cantidad actual a la que nos pasan es negativa, 
+la cantidad de la cuenta pasa a ser 0.
+*/
+
+class Cuenta {
+
+Cuenta({
+  required this.titular, 
+  this.cantidad, 
+});
+
+String titular;
+double cantidad;
+
+String get info{
+  return 'Cuenta de $titular saldo $cantidad'
+}
+
+void ingresar(double cantidad){
+  if (cantidad < 0) {
+    return;
+  }
+}
+
+void ingresar(double cantidad){
+  if (nueva cantidad >0 )
+  return (double cantidad =0) ;
+}
+}
+
+
+
+
